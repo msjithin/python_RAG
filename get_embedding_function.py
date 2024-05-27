0c079e526758e6d489e31c20ef368f36dde9f741
msjithin <msjithin108@gmail.com>
2024-05-26 21:11:33 -0700

    updates model to llama3

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -6,5 +6,5 @@ def get_embedding_function():
     # embeddings = BedrockEmbeddings(
     #     credentials_profile_name="default", region_name="us-east-1"
     # )
-    embeddings = OllamaEmbeddings(model="llama2")
+    embeddings = OllamaEmbeddings(model="llama3")
     return embeddings
